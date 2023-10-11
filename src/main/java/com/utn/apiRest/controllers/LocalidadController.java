56a7d440f3ebb6b50f9e3334ba076ec3ff028cad
Carlos Gutierrez <96015610+CharlysHD@users.noreply.github.com>
2023-10-11 15:36:33 -0300

    v2

---
+++ src/main/java/com/utn/apiRest/controllers/LocalidadController.java
@@ -0,0 +1,16 @@
+package com.utn.apiRest.controllers;
+
+import com.utn.apiRest.entidades.Localidad;
+import com.utn.apiRest.servicios.LocalidadServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "api/v1/localidades")
+public class LocalidadController extends BaseControllerImpl<Localidad, LocalidadServiceImpl>{
+
+}
